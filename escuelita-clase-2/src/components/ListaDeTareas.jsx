d4695a6116983e3f29e552a586cee2e00496861f
vitipe <victorcasbas@gmail.com>
2021-07-27 02:18:36 -0300

    Acomoda algunos detalles estéticos

--- escuelita-clase-2/src/components/ListaDeTareas.jsx
+++ escuelita-clase-2/src/components/ListaDeTareas.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Boton from './Boton';
 
-
-
 export default function ListaDeTareas(props) {
   const [tareas, setTareas] = React.useState(props.arrayTareas);
 
   const listaDeTareas = tareas.map((tarea, i) =>
     <div key={i} id={i}>
-      <li >{tarea}<Boton type='button' texto='Borrar'id={"boton" + i} onClick={() => setTareas(tareas.filter((tarea) => tarea !== tareas[i])) }/></li>
+      <li >{tarea}<Boton type='button' texto='Borrar'id={"boton" + i} onClick={() => setTareas(tareas.filter((tarea) => tarea !== tareas[i]))}/></li>
     </div>
   );
 
